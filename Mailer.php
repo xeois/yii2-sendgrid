c344029326175b38dbb0af80c635471bac5602be
Mikalai Shershan <shnm@localhost.localdomain>
2015-11-17 11:40:08 +0300

    format && documentation fix

--- Mailer.php
+++ Mailer.php
@@ -48,120 +48,120 @@ use Yii;
  */
 class Mailer extends BaseMailer
 {
-    /**
-     * @var string message default class name.
-     */
-    public $messageClass = 'shershennm\sendgrid\Message';
-    /**
-     * @var string the username for the sendgrid api
-     */
-    public $username;
-    /**
-     *
-     * @var string the password for the sendgrid api
-     */
-    public $password;
-    /**
-     * @var string key for the sendgrid api
-     */
-    public $key;
-    /**
-     * @var array a list of options for the sendgrid api
-     */
-    public $options = [];
-    /**
-     * @var string a json string of the raw response from the sendgrid
-     */
-    private $_rawResponse;
-    /**
-     * @var array a list of errors
-     */
-    private $_errors = [];
-    /**
-     * @var string Send grid mailer instance
-     */
-    private $_sendGridMailer;
+	/**
+	 * @var string message default class name.
+	 */
+	public $messageClass = 'shershennm\sendgrid\Message';
+	/**
+	 * @var string the username for the sendgrid api
+	 */
+	public $username;
+	/**
+	 *
+	 * @var string the password for the sendgrid api
+	 */
+	public $password;
+	/**
+	 * @var string key for the sendgrid api
+	 */
+	public $key;
+	/**
+	 * @var array a list of options for the sendgrid api
+	 */
+	public $options = [];
+	/**
+	 * @var string a json string of the raw response from the sendgrid
+	 */
+	private $_rawResponse;
+	/**
+	 * @var array a list of errors
+	 */
+	private $_errors = [];
+	/**
+	 * @var string Send grid mailer instance
+	 */
+	private $_sendGridMailer;
 
-    /**
-     * @return \SendGrid Send grid mailer instance
-     */
-    public function getSendGridMailer()
-    {
-        if (!is_object($this->_sendGridMailer)) {
-            $this->_sendGridMailer = $this->createSendGridMailer();
-        }
+	/**
+	 * @return \SendGrid Send grid mailer instance
+	 */
+	public function getSendGridMailer()
+	{
+		if (!is_object($this->_sendGridMailer)) {
+			$this->_sendGridMailer = $this->createSendGridMailer();
+		}
 
-        return $this->_sendGridMailer;
-    }
+		return $this->_sendGridMailer;
+	}
 
-    /**
-     * Create send grid mail instance with stored params
-     * @return \SendGrid
-     * @throws \yii\base\InvalidConfigException
-     */
-    public function createSendGridMailer()
-    {
-        if ($this->key) {
-            $sendgrid = new \SendGrid($this->key, $options);
-        } elseif ($this->username && $this->password) {
-            $sendgrid = new \SendGrid($this->username, $this->password, $options);
-        } else {
-            throw new InvalidConfigException("You must configure mailer.");
-        }
+	/**
+	 * Create send grid mail instance with stored params
+	 * @return \SendGrid
+	 * @throws \yii\base\InvalidConfigException
+	 */
+	public function createSendGridMailer()
+	{
+		if ($this->key) {
+			$sendgrid = new \SendGrid($this->key, $options);
+		} elseif ($this->username && $this->password) {
+			$sendgrid = new \SendGrid($this->username, $this->password, $options);
+		} else {
+			throw new InvalidConfigException("You must configure mailer.");
+		}
 
-        return $sendgrid;
-    }
+		return $sendgrid;
+	}
 
-    /**
-     * @inheritdoc
-     */
-    public function sendMessage($message)
-    {
-        $this->setRawResponse($this->sendGridMailer->send($message->sendGridMessage));
-        $responseArray = Json::decode($this->getRawResponse());
+	/**
+	 * @inheritdoc
+	 */
+	public function sendMessage($message)
+	{
+		$this->setRawResponse($this->sendGridMailer->send($message->sendGridMessage));
+		$responseArray = Json::decode($this->getRawResponse());
 
-        if (!isset($responseArray['message'])) {
-            throw new \Exception('Invalid SendGrid response format');
-        } elseif ($responseArray['message'] === "success") {
-            // reset the error if success
-            $this->setErrors(array());
-            return true;
-        } elseif (isset($responseArray['errors'])) {
-            // reset the error if success
-            $this->setErrors($responseArray['errors']);
-            return false;
-        }
-    }
+		if (!isset($responseArray['message'])) {
+			throw new \Exception('Invalid SendGrid response format');
+		} elseif ($responseArray['message'] === "success") {
+			// reset the error if success
+			$this->setErrors(array());
+			return true;
+		} elseif (isset($responseArray['errors'])) {
+			// reset the error if success
+			$this->setErrors($responseArray['errors']);
+			return false;
+		}
+	}
 
-    /**
-     * @return string get the raw response, this can be a json string or empty string
-     */
-    public function getRawResponse()
-    {
-        return $this->_rawResponse;
-    }
+	/**
+	 * @return string get the raw response, this can be a json string or empty string
+	 */
+	public function getRawResponse()
+	{
+		return $this->_rawResponse;
+	}
 
-    /**
-     * @param string $value set a raw response, the response get from [[sendMessage()]] is an object, convert it to json
-     */
-    public function setRawResponse($value)
-    {
-        $this->_rawResponse = Json::encode($value);
-    }
+	/**
+	 * @param string $value set a raw response, the response get from [[sendMessage()]] is an object, convert it to json
+	 */
+	public function setRawResponse($value)
+	{
+		$this->_rawResponse = Json::encode($value);
+	}
 
-    /**
-     * @return array a list of errors, the response get [[sendMessage()]]
-     */
-    public function getErrors()
-    {
-        return $this->_errors;
-    }
+	/**
+	 * @return array a list of errors, the response get [[sendMessage()]]
+	 */
+	public function getErrors()
+	{
+		return $this->_errors;
+	}
 
-    /**
-     * @param array $errors a array of errors
-     */
-    public function setErrors($errors)
-    {
-        $this->_errors = $errors;
-    }
+	/**
+	 * @param array $errors a array of errors
+	 */
+	public function setErrors($errors)
+	{
+		$this->_errors = $errors;
+	}
 } 
--- Message.php
+++ Message.php
@@ -13,234 +13,244 @@ use yii\helpers\BaseArrayHelper;
 
 class Message extends BaseMessage
 {
-    private $_sendGridMessage;
+	private $_sendGridMessage;
 
-    public function getSendGridMessage()
-    {
-        if ($this->_sendGridMessage == null) {
-            $this->_sendGridMessage = new \SendGrid\Email();
-        }
-        return $this->_sendGridMessage;
-    }
+	public function getSendGridMessage()
+	{
+		if ($this->_sendGridMessage == null) {
+			$this->_sendGridMessage = new \SendGrid\Email();
+		}
+		return $this->_sendGridMessage;
+	}
 
-    /**
-     * @inheritdoc
-     */
-    public function getCharset()
-    {
-        // not available on sendgrid
-    }
+	/**
+	 * @inheritdoc
+	 */
+	public function getCharset()
+	{
+		// not available on sendgrid
+	}
 
-    /**
-     * @inheritdoc
-     */
-    public function setCharset($charset)
-    {
-        // not available on sendgrid
-    }
+	/**
+	 * @inheritdoc
+	 */
+	public function setCharset($charset)
+	{
+		// not available on sendgrid
+	}
 
-    /**
-     * @inheritdoc
-     */
-    public function getFrom()
-    {
-        return $this->sendGridMessage->getFrom();
-    }
+	/**
+	 * @inheritdoc
+	 */
+	public function getFrom()
+	{
+		return $this->sendGridMessage->getFrom();
+	}
 
-    /**
-     * @inheritdoc
-     */
-    public function setFrom($from)
-    {
-        $this->addEmailParam($from, 'from');
+	/**
+	 * @inheritdoc
+	 */
+	public function setFrom($from)
+	{
+		$this->addEmailParam($from, 'from');
 
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * @inheritdoc
-     */
-    public function getReplyTo()
-    {
-        return $this->sendGridMessage->getReplyTo();
-    }
+	/**
+	 * @inheritdoc
+	 */
+	public function getReplyTo()
+	{
+		return $this->sendGridMessage->getReplyTo();
+	}
 
-    /**
-     * @inheritdoc
-     */
-    public function setReplyTo($replyTo)
-    {
-        $this->sendGridMessage->setReplyTo($replyTo);
+	/**
+	 * @inheritdoc
+	 */
+	public function setReplyTo($replyTo)
+	{
+		$this->sendGridMessage->setReplyTo($replyTo);
 
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * @inheritdoc
-     */
-    public function getTo()
-    {
-        return $this->sendGridMessage->to;
-    }
+	/**
+	 * @inheritdoc
+	 */
+	public function getTo()
+	{
+		return $this->sendGridMessage->to;
+	}
 
-    /**
-     * @inheritdoc
-     */
-    public function setTo($to)
-    {
-        $this->addEmailParam($to, 'to');
+	/**
+	 * @inheritdoc
+	 */
+	public function setTo($to)
+	{
+		$this->addEmailParam($to, 'to');
 
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * @inheritdoc
-     */
-    public function getCc()
-    {
-        return $this->sendGridMessage->getCcs();
-    }
+	/**
+	 * @inheritdoc
+	 */
+	public function getCc()
+	{
+		return $this->sendGridMessage->getCcs();
+	}
 
-    /**
-     * @inheritdoc
-     */
-    public function setCc($cc)
-    {
-        $this->addEmailParam($cc, 'cc');
+	/**
+	 * @inheritdoc
+	 */
+	public function setCc($cc)
+	{
+		$this->addEmailParam($cc, 'cc');
 
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * @inheritdoc
-     */
-    public function getBcc()
-    {
-        return $this->sendGridMessage->getBccs();
-    }
+	/**
+	 * @inheritdoc
+	 */
+	public function getBcc()
+	{
+		return $this->sendGridMessage->getBccs();
+	}
 
-    /**
-     * @inheritdoc
-     */
-    public function setBcc($bcc)
-    {
-        $this->addEmailParam($bcc, 'bcc');
+	/**
+	 * @inheritdoc
+	 */
+	public function setBcc($bcc)
+	{
+		$this->addEmailParam($bcc, 'bcc');
 
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * @inheritdoc
-     */
-    public function getSubject()
-    {
-        return $this->sendGridMessage->getSubject();
-    }
+	/**
+	 * @inheritdoc
+	 */
+	public function getSubject()
+	{
+		return $this->sendGridMessage->getSubject();
+	}
 
-    /**
-     * @inheritdoc
-     */
-    public function setSubject($subject)
-    {
-        $this->sendGridMessage->setSubject($subject);
+	/**
+	 * @inheritdoc
+	 */
+	public function setSubject($subject)
+	{
+		$this->sendGridMessage->setSubject($subject);
 
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * @inheritdoc
-     */
-    public function setTextBody($text)
-    {
-        $this->sendGridMessage->setText($text);
+	/**
+	 * @inheritdoc
+	 */
+	public function setTextBody($text)
+	{
+		$this->sendGridMessage->setText($text);
 
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * @inheritdoc
-     */
-    public function setHtmlBody($html)
-    {
-        $this->sendGridMessage->setHtml($html);
+	/**
+	 * @inheritdoc
+	 */
+	public function setHtmlBody($html)
+	{
+		$this->sendGridMessage->setHtml($html);
 
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * @inheritdoc
-     */
-    public function attach($fileName, array $options = [])
-    {
-        $this->sendGridMessage->addAttachment($fileName);
+	/**
+	 * @inheritdoc
+	 */
+	public function attach($fileName, array $options = [])
+	{
+		$this->sendGridMessage->addAttachment($fileName);
 
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * @inheritdoc
-     */
-    public function attachContent($content, array $options = [])
-    {
-        // no available method for sendgrid
-    }
+	/**
+	 * @inheritdoc
+	 */
+	public function attachContent($content, array $options = [])
+	{
+		// no available method for sendgrid
+	}
 
-    /**
-     * @inheritdoc
-     */
-    public function embed($fileName, array $options = [])
-    {
-        // no available method for sendgrid
-    }
+	/**
+	 * @inheritdoc
+	 */
+	public function embed($fileName, array $options = [])
+	{
+		// no available method for sendgrid
+	}
 
-    /**
-     * @inheritdoc
-     */
-    public function embedContent($content, array $options = [])
-    {
-        // no available method for sendgrid
-    }
+	/**
+	 * @inheritdoc
+	 */
+	public function embedContent($content, array $options = [])
+	{
+		// no available method for sendgrid
+	}
 
-    /**
-     * @inheritdoc
-     */
-    public function toString()
-    {
-        $string = '';
-        foreach ($this->sendGridMessage->toWebFormat() as $key => $value) {
-            $string .= sprintf("%s:%s\n", $key, $value);
-        }
-        return $string;
-    }
+	/**
+	 * @inheritdoc
+	 */
+	public function toString()
+	{
+		$string = '';
+		foreach ($this->sendGridMessage->toWebFormat() as $key => $value) {
+			$string .= sprintf("%s:%s\n", $key, $value);
+		}
+		return $string;
+	}
 
-    private function addEmailParam($paramValue, $paramType)
-    {
-        $paramTypeName = $paramType . 'Name';
+	/**
+	 * Adding to sendgrid params which coontains email new items
+	 * @param string|array $paramValue ['email' => 'name'] or ['email', ['email' => 'name'], 'email'] or 'email'
+	 * @inheritdoc yii\mail\MessageInterface for more info
+	 * @param string $paramType sendGrid var name like cc, bcc, to, from
+	 */
+	private function addEmailParam($paramValue, $paramType)
+	{
+		$paramTypeName = $paramType . 'Name';
 
-        $this->sendGridMessage->$paramType = [];
-        $this->sendGridMessage->$paramTypeName = [];
+		$this->sendGridMessage->$paramType = [];
+		$this->sendGridMessage->$paramTypeName = [];
 
-        if (!is_array($paramValue) || BaseArrayHelper::isAssociative($paramValue)) {
-            $this->addSingleParam($paramValue, $paramType);
-        } else {
-            foreach ($paramValue as $value) {
-                $this->addSingleParam($paramValue, $paramType);
-            }
-        }
+		if (!is_array($paramValue) || BaseArrayHelper::isAssociative($paramValue)) {
+			$this->addSingleParam($paramValue, $paramType);
+		} else {
+			foreach ($paramValue as $value) {
+				$this->addSingleParam($paramValue, $paramType);
+			}
+		}
 
-        return $this;
-    }
+		return $this;
+	}
+	
+	/**
+	 * @param string|array $paramValue ['email' => 'name'] or 'email'
+	 * @param string $paramType sendGrid var name like cc, bcc, to, from
+	 */
+	private function addSingleParam($paramValue, $paramType)
+	{
+		$addFunction = 'add' . ucfirst($paramType);
 
-    private function addSingleParam($paramValue, $paramType)
-    {
-        $addFunction = 'add' . ucfirst($paramType);
-
-        if (BaseArrayHelper::isAssociative($paramValue)) {
-            $this->sendGridMessage->$addFunction(key($paramValue), current($paramValue));
-        }
-        else
-        {
-            $this->$addFunction($paramValue);
-        }
-    }
+		if (is_array($paramValue) && BaseArrayHelper::isAssociative($paramValue)) {
+			$this->sendGridMessage->$addFunction(key($paramValue), current($paramValue));
+		}
+		else
+		{
+			$this->$addFunction($paramValue);
+		}
+	}
 } 
